6b30f2574e712ee4b3c0a13b8e4bab57f79dbef3
Void Drifter <135348722+VoltacceptYT@users.noreply.github.com>
2024-10-19 18:11:31 -0400

    Update CookieValley.user.js

--- CookieValley.user.js
+++ CookieValley.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Cookie Valley Userscript
 // @namespace    https://github.com/VoltacceptYT/cookievalley
-// @version      v0.2.7
+// @version      v0.3.1
 // @description  Install the Cookie Valley Mod on the Cookie Clicker Web!
 // @author       Void Drifter, Samantha Stahlke
 // @icon         https://raw.githubusercontent.com/VoltacceptYT/cookievalley/refs/heads/main/img/modicon.png
@@ -10,59 +10,62 @@
 // @grant        none
 // ==/UserScript==
 
-
-
-
 (function () {
 	'use strict';
 
 	Game.registerMod('CookieValley Web Enabler', {
-		init: function () {
-			if (!document.getElementById('cookie_valley_enabler')) {
-				const originalUpdateMenu = Game.UpdateMenu;
-				Game.UpdateMenu = function () {
-					originalUpdateMenu();
-					if (Game.onMenu === 'prefs') {
-						const menu = document.getElementById('menu');
-						let section = Array.from(menu.getElementsByClassName('subsection')).find(subsection => subsection.innerText.includes('Cookie Valley Web'));
-						if (!section) {
-							section = document.createElement('div');
-							section.className = 'subsection';
-							section.innerHTML = '<div class="title">Cookie Valley Web</h2>';
-							const lastSubsection = menu.querySelector('.subsection:last-of-type');
-							lastSubsection.insertAdjacentElement('afterend', section);
-						}
-						if (!document.getElementById('cookie_valley_enabler')) {
-							const str = '<div id="cookie_valley_enabler" class="listing">' + Game.WritePrefButton('hasCV', 'CookieValleyEnabler', 'Cookie Valley ON', 'Cookie Valley OFF', '', false).replace("PlaySound('snd/tick.mp3');", "PlaySound('snd/tick.mp3');Game.mods['CookieValley Web Enabler'].toggle()") + '</div>';
-							section.insertAdjacentHTML('beforeend', str);
-						}
+			init: function () {
+					if (!document.getElementById('cookie_valley_enabler')) {
+							const originalUpdateMenu = Game.UpdateMenu;
+							Game.UpdateMenu = function () {
+									originalUpdateMenu();
+									if (Game.onMenu === 'prefs') {
+											const menu = document.getElementById('menu');
+											let section = Array.from(menu.getElementsByClassName('subsection')).find(subsection => subsection.innerText.includes('Cookie Valley Web'));
+											if (!section) {
+													section = document.createElement('div');
+													section.className = 'block';
+													section.style.padding = '0px';
+													section.style.margin = '8px 4px';
+													section.innerHTML = `
+															<div class="subsection">
+																	<div class="title">Cookie Valley Web</div>
+																	<div id="cookie_valley_enabler" class="listing">
+																			<a class="smallFancyButton prefButton option off" id="CookieValleyEnabler"
+																				 onclick="Game.Toggle('hasCV','CookieValleyEnabler','Cookie Valley ON','Cookie Valley OFF','0');PlaySound('snd/tick.mp3');Game.mods['CookieValley Web Enabler'].toggle()">Cookie Valley OFF</a>
+																	</div>
+															</div>`;
+													const heightDiv = menu.querySelector('div[style="height:128px;"]');
+													heightDiv.insertAdjacentElement('beforebegin', section);
+											}
+									}
+							};
+							Game.prefs.hasCV = 0;
 					}
-				};
-				Game.prefs.hasCV = 0;
-			}
-		},
-		toggle: function () {
-			if (Game.prefs.hasCV) {
-				if (!document.getElementById('cookie_valley')) {
-					fetch('https://raw.githubusercontent.com/voltacceptyt/cookievalley/refs/heads/main/main.js')
-						.then(response => response.text())
-						.then(data => {
-							const script = document.createElement('script');
-							script.textContent = data;
-							script.id = 'cookie_valley';
-							document.body.appendChild(script);
-						})
-						.catch(error => console.error('Error Fetching Mod:', error));
-				}
-			} else {
-				const script = document.getElementById('cookie_valley');
-				if (script) {
-					Game.toSave = true;
-					document.body.removeChild(script);
-					location.reload(true)
-				}
-			}
-		},
+			},
+			toggle: function () {
+					if (Game.prefs.hasCV) {
+							if (!document.getElementById('cookie_valley')) {
+									fetch('https://raw.githubusercontent.com/voltacceptyt/cookievalley/refs/heads/main/main.js')
+											.then(response => response.text())
+											.then(data => {
+													const script = document.createElement('script');
+													script.textContent = data;
+													script.id = 'cookie_valley';
+													document.body.appendChild(script);
+											})
+											.catch(error => console.error('Error Fetching Mod:', error));
+							}
+					} else {
+							const script = document.getElementById('cookie_valley');
+							if (script) {
+									Game.toSave = true;
+									document.body.removeChild(script);
+									setTimeout(() => {
+											location.reload(true);
+									}, 250);
+							}
+					}
+			},
 	});
 })();
-
