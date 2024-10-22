6e6b868777f8fe8f91214a31cf75b24925050a9c
Void Drifter <135348722+VoltacceptYT@users.noreply.github.com>
2024-10-22 17:51:24 -0400

    Update CookieValley.user.js

--- CookieValley.user.js
+++ CookieValley.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Red Bread Crossover Userscript
 // @namespace    https://github.com/VoltacceptYT/redbreadcrossover
-// @version      v0.5.7
+// @version      v0.5.8
 // @description  Install the Cookie Valley Mod on the Cookie Clicker Web!
 // @author       Void Drifter, Samantha Stahlke
 // @icon         https://voltacceptyt.github.io/redbreadcrossover/img/modicon.png
@@ -994,13 +994,16 @@
       },
     });
   });
-  setInterval(() => {
-    var style = document.getElementById('RedBreadStyle');
-    style.textContent += `
-    #rowCanvas15 {
-      height:` + (200 / window.innerHeight) * 100 + `%;
-    }`
-  }, 100);
+  
+  if (typeof Game !== 'undefined' && Game.ready) {
+    setInterval(() => {
+      var style = document.getElementById('RedBreadStyle');
+      style.textContent += `
+      #rowCanvas15 {
+        height:` + (200 / document.getElementById('centerArea').style.height) * 100 + `%;
+      }`
+    }, 100);
+  }
 })();
 
 //Your Not Supposed to be Here...
