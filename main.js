d63b28f2fba3f8b35cc66b3f2947325e743ca339
Void Drifter <135348722+VoltacceptYT@users.noreply.github.com>
2024-10-22 09:32:50 -0400

    Update main.js

--- main.js
+++ main.js
@@ -171,7 +171,7 @@ Game.registerMod("RedBreadCrossover", {
     }`;
     document.getElementsByTagName('head')[0].appendChild(style);
 
-    function waitForElement(id, callback) {
+    Game.waitForElement = function (id, callback) {
       var checkExist = setInterval(function () {
         if (document.getElementById(id)) {
           clearInterval(checkExist);
@@ -182,12 +182,12 @@ Game.registerMod("RedBreadCrossover", {
 
     const oldCanvas = document.getElementById('rowCanvas15');
     const newCanvas = document.createElement('canvas');
+    newCanvas.className = 'rowCanvas';
+    newCanvas.id = 'rowCanvas15';
+    newCanvas.width = 434;
+    newCanvas.height = 148;
 
-    waitForElement('rowCanvas15', function () {
-      newCanvas.className = 'rowCanvas';
-      newCanvas.id = 'rowCanvas15';
-      newCanvas.width = 434;
-      newCanvas.height = 148;
+    Game.waitForElement('rowCanvas15', function () {
       oldCanvas.replaceWith(newCanvas);
     });
 
