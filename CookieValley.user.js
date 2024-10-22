6ce4f091156af38f71d8a9dfc1343f58aa637d1a
Void Drifter <135348722+VoltacceptYT@users.noreply.github.com>
2024-10-22 12:19:49 -0400

    Update CookieValley.user.js

--- CookieValley.user.js
+++ CookieValley.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Red Bread Crossover Userscript
 // @namespace    https://github.com/VoltacceptYT/redbreadcrossover
-// @version      v0.3.7
+// @version      v0.3.8
 // @description  Install the Cookie Valley Mod on the Cookie Clicker Web!
 // @author       Void Drifter, Samantha Stahlke
 // @icon         https://voltacceptyt.github.io/redbreadcrossover/img/modicon.png
@@ -11,27 +11,29 @@
 // ==/UserScript==
 
 (function () {
-	'use strict';
-	fetch('https://voltacceptyt.github.io/redbreadcrossover/main.js')
-		.then(response => response.text())
-		.then(data => {
-			const script = document.createElement('script');
-			script.textContent = data;
-			document.body.appendChild(script);
-		});
-    function checkGameLoaded() {
-      if (typeof Game !== 'undefined' && Game.ready && Game.customBuildingsEnabled) {
-          var canvases = document.querySelectorAll('.rowCanvas');
-          canvases.forEach(function (canvas) {
-              canvas.className = canvas.className;
-              canvas.id = canvas.id;
-              canvas.width = canvas.width;
-              canvas.height = canvas.height  * 2;
-          });
-      } else {
-          setTimeout(checkGameLoaded, 100);
-      }
+  'use strict';
+  fetch('https://voltacceptyt.github.io/redbreadcrossover/main.js')
+    .then(response => response.text())
+    .then(data => {
+      const script = document.createElement('script');
+      script.textContent = data;
+      document.body.appendChild(script);
+    });
+  function checkGameLoaded() {
+    if (typeof Game !== 'undefined' && Game.ready && Game.customBuildingsEnabled) {
+      var canvas = document.getElementById('rowCanvas15');
+      var newCanvas = document.createElement('canvas');
+      newCanvas.className = canvas.className;
+      newCanvas.id = canvas.id;
+      newCanvas.width = canvas.width;
+      newCanvas.height = canvas.height * 2;
+
+      var data = newCanvas.getContext('2d')
+      data.drawImage(canvas.toDataURL())
+    } else {
+      setTimeout(checkGameLoaded, 100);
+    }
   }
-  
+
   checkGameLoaded();
 })();
