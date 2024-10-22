9e528e860f4b6f78e7c8b44ca5a994e27c4c2161
Void Drifter <135348722+VoltacceptYT@users.noreply.github.com>
2024-10-22 10:33:25 -0400

    Update CookieValley.user.js

--- CookieValley.user.js
+++ CookieValley.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Red Bread Crossover Userscript
 // @namespace    https://github.com/VoltacceptYT/redbreadcrossover
-// @version      v0.3.3
+// @version      v0.3.4
 // @description  Install the Cookie Valley Mod on the Cookie Clicker Web!
 // @author       Void Drifter, Samantha Stahlke
 // @icon         https://voltacceptyt.github.io/redbreadcrossover/img/modicon.png
@@ -16,7 +16,29 @@
 		.then(response => response.text())
 		.then(data => {
 			const script = document.createElement('script');
-			script.textContent = data;
+			script.textContent = data + `
+    		document.getElementById('rowCanvas15').addEventListener('load', function () {
+      		function checkGameLoaded() {
+        		if (Game.ready) {
+          		if (Game.customBuildingsEnabled) {
+            		var canvas = document.getElementById('rowCanvas15');
+            		if (canvas) {
+              		canvas.height = 168;
+		              console.log('The Building\'s Canvas height has been extended!');
+    		        } else {
+        		      console.error('The Building hasn\'t been unlocked!');
+		            }
+    		      } else {
+        		    console.error('The use of Custom Buildings isn\'t enabled!')
+		          }
+    		    } else {
+        		  setTimeout(checkGameLoaded, 100);
+		        }
+    		  }
+
+    		  checkGameLoaded();
+    		});
+    `;
 			document.body.appendChild(script);
 		});
 })();
