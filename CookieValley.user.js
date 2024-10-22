a785bb025748725c6cf0f240b97a352bc14a94d4
Void Drifter <135348722+VoltacceptYT@users.noreply.github.com>
2024-10-22 11:34:34 -0400

    Update CookieValley.user.js

--- CookieValley.user.js
+++ CookieValley.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Red Bread Crossover Userscript
 // @namespace    https://github.com/VoltacceptYT/redbreadcrossover
-// @version      v0.3.5
+// @version      v0.3.6
 // @description  Install the Cookie Valley Mod on the Cookie Clicker Web!
 // @author       Void Drifter, Samantha Stahlke
 // @icon         https://voltacceptyt.github.io/redbreadcrossover/img/modicon.png
@@ -19,28 +19,27 @@
 			script.textContent = data;
 			document.body.appendChild(script);
 		});
-		function checkGameLoaded() {
-			if (typeof Game !== 'undefined' && Game.ready) {
-				// Get all canvas elements with the class 'rowCanvas'
-				var canvases = document.querySelectorAll('.rowCanvas');
-				canvases.forEach(function (canvas) {
-					// Create a new canvas element with the same attributes but a taller height
-					var newCanvas = document.createElement('canvas');
-					newCanvas.className = canvas.className;
-					newCanvas.id = canvas.id;
-					newCanvas.width = canvas.width;
-					newCanvas.height = canvas.height + 40; // Increase height by 20px
+	function checkGameLoaded() {
+		if (typeof Game !== 'undefined' && Game.ready) {
+			var canvases = document.querySelectorAll('.rowCanvas');
+			canvases.forEach(function (canvas) {
+				var newCanvas = document.createElement('canvas');
+				newCanvas.className = canvas.className;
+				newCanvas.id = canvas.id;
+				newCanvas.width = canvas.width;
+				newCanvas.height = canvas.height + 40;
 
-					// Replace the old canvas with the new one
-					canvas.replaceWith(newCanvas);
-				});
-				console.log('All building row canvases have been replaced with taller canvases.');
-			} else {
-				// Retry after a short delay if the game is not ready
-				setTimeout(checkGameLoaded, 100);
-			}
+				var context = newCanvas.getContext('2d');
+				context.drawImage(canvas, 0, 0);
+
+				canvas.replaceWith(newCanvas);
+			});
+			console.log('All building row canvases have been replaced with taller canvases and redrawn.');
+		} else {
+			setTimeout(checkGameLoaded, 100);
 		}
+	}
 
-		// Start checking if the game has loaded
-		checkGameLoaded();
+	checkGameLoaded();
+
 })();
