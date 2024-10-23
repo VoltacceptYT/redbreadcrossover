f810536891622de07463a65085c941885359f1a4
Void Drifter <135348722+VoltacceptYT@users.noreply.github.com>
2024-10-23 08:42:51 -0400

    Update mainDebug.js

--- debug/mainDebug.js
+++ debug/mainDebug.js
@@ -1,3 +1,6 @@
+//DEBUG CMDS
+
+//addCookies(Amount): adds X Trillion Amount of Cookies to Character
 function addCookies(Amount) {
   let Convert2Tri = Math.pow(10, 18) * Amount;
   Game.Cookies += Convert2Tri;
